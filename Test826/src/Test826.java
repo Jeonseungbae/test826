86410643f4f9e03cf4f5d28b83bbaa1cd483f2ef
Jeon <Jeon@DESKTOP-FMIK8D5>
2021-12-13 19:19:44 +0900

    버전 : 3.0 변경 : 버전 업그레이드 및 로그인 기능 개선 작성자 : 전승배

--- Test826/src/Test826.java
+++
@@ -1,8 +0,0 @@
-
-public class Test826 {
-	public static void main(String[] args) {
-		System.out.println("Hello GIT World!");
-		System.out.println("Version : 1.0");
-		System.out.println("·Î±×ÀÎ µÊ!!");
-	}
-}
---
+++ Test826/src/Testjava2.java
@@ -0,0 +1,8 @@
+
+public class Testjava2 {
+
+	public static void main(String[] args) {
+		System.out.println("새로운 java 코드");
+	}
+
+}
