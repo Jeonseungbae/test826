79b6bd9da65e8ff0bac862e07cd824ad2c2d9dbe
Jeon <Jeon@DESKTOP-FMIK8D5>
2021-12-13 18:17:57 +0900

    버전 : 2.0 변경내용 : 로그인 기능 업그레이드 작성자 : 전승배

--- Test826/src/Test826.java
+++ Test826/src/Test826.java
@@ -3,5 +3,6 @@ public class Test826 {
 	public static void main(String[] args) {
 		System.out.println("Hello GIT World!");
 		System.out.println("Version : 1.0");
+		System.out.println("·Î±×ÀÎ µÊ!!");
 	}
 }
